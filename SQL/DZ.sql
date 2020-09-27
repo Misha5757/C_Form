31388a166dcf9dfb2d2cf55160ae18dcc34313e4
Misha5757 <ra107696@gmail.com>
2020-09-27 12:08:26 +0300

    Исп ошибки

--- SQL/DZ.sql
+++ SQL/DZ.sql
@@ -1,38 +1,39 @@
-create database [Magaz333]
-use [Magaz333]
-go
-create table [Knigi]
+CREATE DATABASE [Magaz3333]
+USE [Magaz3333]
+GO
+CREATE TABLE [Knigi]
 (
 	[ID]				INT  IDENTITY (0, 1),
 	[Name]				NVARCHAR(50)		NOT NULL,
 	[Numberofpages]		INT					NOT NULL,
 	[IDGenre]			INT CONSTRAINT FK_Knigi_IDGenre_Genre1_ID
 	FOREIGN KEY REFERENCES [Genre1]([ID]),
-	[IDAvtor]					NCHAR(1) CONSTRAINT FK_Knigi_IDAvtor_Avtor_ID
+	[IDAvtor]					INT CONSTRAINT FK_Knigi_IDAvtor_Avtor_ID
 	FOREIGN KEY REFERENCES [Avtor]([ID]) ,
-	[IDIzdatel]			NCHAR(1) CONSTRAINT FK_Knigi_IDIzdatel_Izdatel_ID
+	[IDIzdatel]			INT CONSTRAINT FK_Knigi_IDIzdatel_Izdatel_ID
 	FOREIGN KEY REFERENCES [Izdatel]([ID]),
+	CONSTRAINT PK_Knigi_ID PRIMARY KEY ([ID])
 )
 go
-create table [Genre1]
+CREATE TABLE [Genre1]
 (
-	[ID]					 INT Not null,	
-	[Name2]					 VARCHAR(50)		 NOT NULL,
+	[ID]					INT  IDENTITY (0, 1),	
+	[Name]					 NVARCHAR(50)		 NOT NULL,
 	CONSTRAINT PK_Genre1_ID PRIMARY KEY ([ID])
 )
 go
-create table [Avtor]
+CREATE TABLE [Avtor]
 (
-	[ID]						NCHAR(1),
+	[ID]						INT  IDENTITY (0, 1),
 	[Familya]					NVARCHAR(50)		NOT NULL,
 	[Imya]						NVARCHAR(50)		NOT NULL,
 	[Otchestvo]					NVARCHAR(50)		NOT NULL,
 	[Data]						DateTime			NOT NULL,
 	CONSTRAINT PK_Avtor_ID PRIMARY KEY ([ID])
 )
-create table [Izdatel]
+CREATE TABLE [Izdatel]
 (
-	[ID]					    NCHAR(1) ,
+	[ID]					    INT  IDENTITY (0, 1) ,
 	[Naimenovanie]				NVARCHAR(50)		NOT NULL,
 	[Adres]						NVARCHAR(50)		NOT NULL,
 	[Strana]					NVARCHAR(50)		NOT NULL,
